8a328bdf6ae551593cac49357e4c9dc9a3196cca
akshatk806 <akshatk806@gmail.com>
2022-10-08 20:33:26 +0530

    7. Adding dark mode in About.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" contact="Contact Us" />
-      <div className="container my-2">
-        <TextForm heading="Enter the text to analyze" />
-      </div>
+      <TextForm heading="Enter the text to analyze" />
+      {/* <About /> */}
     </>
   );
 }
